2c4ac440e264a68d5f7d49f77eb6e59d2e601af5
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2024-03-18 06:01:03 +0000

    update

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.post('/bfhl', (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
